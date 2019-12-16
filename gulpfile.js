38dfd35263dea08dee346cbcf2015d9d97dec7a8
adabaler <58886785+adabaler@users.noreply.github.com>
2019-12-16 12:22:41 +0300

    inital

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('first', function() {
+    return gulp.src('./scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./'))
+})
+gulp.task('watch', function() {
+    gulp.watch('./*.html', gulp.series('first'))
+    gulp.watch('./scss/**/*.scss', gulp.series('first'))
+    gulp.watch('./js/**/*.js', gulp.series('first'))
+})
+gulp.task('default', gulp.series('watch'));
