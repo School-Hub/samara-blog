f98343c80e74dcc86bf375f1b2ed5a96ae60577e
bufgix <ooruc471@yandex.com>
2019-12-18 20:29:58 +0300

    gulp-server eklendi, root da buluanan main.css ./css/ dizinine taşındı

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-gulp.task('first', function() {
-    return gulp.src('./scss/main.scss')
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var server = require("gulp-webserver");
+
+gulp.task("first", function() {
+  return gulp
+    .src("./scss/main.scss")
     .pipe(sass())
-    .pipe(gulp.dest('./'))
-})
-gulp.task('watch', function() {
-    gulp.watch('./*.html', gulp.series('first'))
-    gulp.watch('./scss/**/*.scss', gulp.series('first'))
-    gulp.watch('./js/**/*.js', gulp.series('first'))
-})
-gulp.task('default', gulp.series('watch'));
+    .pipe(gulp.dest("./css/"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./*.html", gulp.series("first"));
+  gulp.watch("./scss/**/*.scss", gulp.series("first"));
+  gulp.watch("./js/**/*.js", gulp.series("first"));
+});
+
+gulp.task("server", function() {
+  gulp.src(".").pipe(
+    server({
+      livereload: true,
+      open: true, // Otomatik tarayıcı acacak
+      port: 8080  // http://localhost:8080
+    })
+  );
+});
+
+gulp.task("default", gulp.parallel("watch", "server"));
